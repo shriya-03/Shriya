0c30bbd9508a5a145606c2ee267ec5b6388c9a10
Shriya S <103843531+shriya-03@users.noreply.github.com>
2025-06-05 18:48:21 +0530

    Add files via upload

--- 5june.py
+++ 5june.py
@@ -15,3 +15,23 @@ def two_sum(nums, target):
 nums = [2, 7, 11, 15]
 target = 9
 print(two_sum(nums, target))  # Output: [0, 1]
+
+#Given an array of integers nums and an integer target, return indices 
+# of the two numbers such that they add up to target. You may assume that 
+# each input would have exactly one solution, and you may not use the same
+# element twice. You can return the answer in any order.
+
+#Output: [0, 1] Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+#Explanation:
+#Data Structure: We use a dictionary (lookup) to record the numbers we have seen so far 
+# along with their indices.
+
+#Complement Calculation: For each number, compute its complement (i.e., target - num).
+
+#Lookup:
+
+#If the complement is already in our dictionary, we immediately return the pair of indices.
+
+#If not, we add the current number and its index into the dictionary.
+
+#Efficiency: This solution works in O(n) time since it only passes through the array once.

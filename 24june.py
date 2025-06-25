117c57106afa079f87282706a65c6adeebfd2bd9
Shriya S <103843531+shriya-03@users.noreply.github.com>
2025-06-25 20:56:26 +0530

    Add files via upload

---
+++ 24june.py
@@ -0,0 +1,11 @@
+def abs_list(nos):
+    return [abs(x) for x in nos]
+
+print(abs_list([3, -7, 0, -2]))  # Output: [3, 7, 0, 2]
+
+#function called abs_list , parameter: nos
+#abs(x) is a built-in Python function.
+#For each number x in the list, it applies abs(x) and creates a new list of those values.
+
+#print(abs_list([3, -7, 0, -2]))
+#This calls the function abs_list with the list [3, -7, 0, -2].

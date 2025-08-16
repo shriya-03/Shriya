6651b0e8db911e6694a6f30de623b1f3347e13ff
Shriya S <103843531+shriya-03@users.noreply.github.com>
2025-08-16 14:53:10 +0530

    Add files via upload

--- 28july.py
+++ 28july.py
@@ -11,11 +11,11 @@ def factorial_digit_square(n: int) -> int:
     total = sum(int(d)**2 for d in fact_str)
     return math.isqrt(total) if math.isqrt(total)**2 == total else math.isqrt(total)+1
 
-print(factorial_digit_square(5))  # 5! = 120 → 1²+2²+0²=5 → sqrt(5)=2.23 → 3
-print(factorial_digit_square(6))  # 6! = 720 → 7²+2²+0²=53 → sqrt(53)=7.28 → 8
+print(factorial_digit_square(5))  # 5! = 120  1²+2²+0²=5  sqrt(5)=2.23  3
+print(factorial_digit_square(6))  # 6! = 720  7²+2²+0²=53  sqrt(53)=7.28 is  8
 
 
 
 # math.isqrt(x) gives the integer part of sqrt(x) (like floor(sqrt(x)))
-# If isqrt(x)**2 == x, then x is a perfect square → return isqrt(x)
+# If isqrt(x)**2 == x, then x is a perfect square  return isqrt(x)
 # Otherwise, we return isqrt(x) + 1 to get the ceiling of the square root.
